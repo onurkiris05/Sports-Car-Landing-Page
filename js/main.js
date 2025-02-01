c60982836fd9ee7b573d6ba30e213defd9888d5e
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-02-01 15:13:15 +0300

    [Added] Scene Change & Media Button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const menuBtn = document.querySelector(".menu");
 const navBar = document.querySelector(".nav-list");
+const bgVideo = document.querySelector(".bg-video");
+const modelTitle = document.querySelector(".model-title");
+const trailer = document.querySelector(".trailer");
+const bgVideoButtons = document.querySelectorAll(".bg-media-btn");
+const screenVideos = document.querySelectorAll(".screen-video");
 
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".carousel");
@@ -20,3 +25,39 @@ const carData = [
   { model: "765LT", video: "/assets/videos/mclaren-4.mp4", color: "#ff2e00" },
   { model: "750S SPIDER", video: "/assets/videos/mclaren-5.mp4", color: "#bbc0dd" },
 ];
+
+function setCurrentCar(carModel) {
+  const currentCar = carData.find((car) => car.model === carModel);
+  modelTitle.textContent = currentCar.model;
+  modelTitle.style.color = currentCar.color;
+  bgVideo.src = currentCar.video;
+  trailer.src = currentCar.video;
+
+  resetBgVideo();
+}
+
+function toggleBgVideo() {
+  bgVideoButtons.forEach((button) => {
+    button.classList.toggle("active");
+  });
+
+  screenVideos.forEach((video) => {
+    video.style.display = bgVideo.paused ? "block" : "none";
+  });
+
+  bgVideo.paused ? bgVideo.play() : bgVideo.pause();
+}
+
+function resetBgVideo() {
+  bgVideoButtons.forEach((button) => {
+    button.classList.remove("active");
+    if (button.classList.contains("pause")) button.classList.add("active");
+  });
+  screenVideos.forEach((video) => {
+    video.style.display = "block";
+  });
+}
+
+bgVideoButtons.forEach((button) => {
+  button.addEventListener("click", toggleBgVideo);
+});
